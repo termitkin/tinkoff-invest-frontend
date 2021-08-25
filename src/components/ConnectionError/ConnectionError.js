2eed1c95bbe5516a69812fc825ad09b13bfec891
termitkin <bigos911@gmail.com>
2021-08-25 17:14:17 +0300

    added component ConnectionError

---
+++ src/components/ConnectionError/ConnectionError.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ConnectionError = () => {
+  return (
+    <div>
+      Что-то пошло не так
+      <br />
+      Нужно обновить страницу.
+      <br />
+      Если не поможет, то перезапустить бекенд
+    </div>
+  );
+};
+
+export default ConnectionError;
